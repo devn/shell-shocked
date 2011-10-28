4791ec910310b1018090825cb87bad78af6ca196
Devin Walters <devinw@gmail.com>
2011-10-28 09:47:33 -0500

    This is just how I roll

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,14 +1,17 @@
-export PATH=~/bin:/usr/local/bin:$PATH
-export CDPATH=.:~/src/personal:~/src/opensource:~/src/bendyworks:~/src/configs:~/Documents
+export CLOJURESCRIPT_HOME=~/git/clojurescript
+export NODE_PATH=/usr/local/lib/node
+PYPATH=:/usr/local/share/python
+# export CLOJURE_HOME=~/git/clojure-1.3.0-beta
+export PATH=$HOME/bin:$PYPATH:/usr/local/bin:/usr/local/sbin:$CLOJURESCRIPT_HOME/bin:$NODE_PATH::$PATH
+# export CDPATH=.:~/src/personal:~/src/opensource:~/src/bendyworks:~/src/configs:~/Documents
 
-export JAVA_HOME=/usr/lib/jvm/java-6-sun
-export JDK_HOME=/usr/lib/jvm/java-6-sun
-export CLOJURE_HOME=~/src/opensource/clojure
+# export JAVA_HOME=/usr/lib/jvm/java-6-sun
+# export JDK_HOME=/usr/lib/jvm/java-6-sun
 
 # Setup terminal, and turn on colors
-export TERM=xterm-color
-export LSCOLORS=gxfxcxdxbxegedabagacad
-export CLICOLOR=1
+# export TERM=xterm-color
+# export LSCOLORS=gxfxcxdxbxegedabagacad
+# export CLICOLOR=1
 
 # Enable color in grep
 export GREP_OPTIONS='--color=auto' 
